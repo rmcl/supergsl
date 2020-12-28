5cfbb48b4b9e28db88f25bbdb442f1ddf5b67f9f
Russell McLoughlin <russ.mcl@gmail.com>
2020-12-28 15:31:44 -0800

    added test for positional slicing

--- supergsl/core/parts/test/test_slice.py
+++ supergsl/core/parts/test/test_slice.py
@@ -57,3 +57,34 @@ class ResolvePartSlicePassTestCase(unittest.TestCase):
         self.assertEquals(ref, reference)
         self.assertEquals(abs_pos, 875)
 
+    def test_visit_part_node(self):
+        reference, pre_slice_part = self.fixtures.mk_part('P3', 500)
+        pre_slice_part.provider.get_child_part_by_slice.return_value = 'SLICED-PART'
+
+        start = AstSlicePosition(-125, PART_SLICE_POSTFIX_END, True)
+        end = AstSlicePosition(0, PART_SLICE_POSTFIX_END, True)
+        slice = AstSlice(start, end)
+
+        part_node = AstPart('TEST', slice, False)
+        part_node.part = pre_slice_part
+
+        rps = ResolvePartSlicePass(None)
+        new_part_node = rps.visit_part_node(part_node)
+
+        pre_slice_part.provider.get_child_part_by_slice.assert_called_once()
+        create_child_args = pre_slice_part.provider.get_child_part_by_slice.call_args.args
+
+        self.assertEquals(create_child_args[0], pre_slice_part)
+        self.assertEquals(create_child_args[1], 'TEST')
+
+        part_start = create_child_args[2]
+        self.assertEquals(part_start.x, -125)
+        self.assertEquals(part_start.approximate, True)
+
+        part_end = create_child_args[3]
+        self.assertEquals(part_end.x, 0)
+        self.assertEquals(part_end.approximate, True)
+
+        self.assertEquals(new_part_node.part, 'SLICED-PART')
+        self.assertEquals(part_node, new_part_node)
+        self.assertEquals(new_part_node.parent_parts, [pre_slice_part])
--- supergsl/core/test/fixtures.py
+++ supergsl/core/test/fixtures.py
@@ -1,17 +1,49 @@
-from supergsl.core.ast import Program, Assembly, Part
-
+import mock
+import random
+from Bio.Seq import Seq
+from supergsl.core.ast import Assembly, Part as AstPart
+from supergsl.core.constants import THREE_PRIME
+from supergsl.core.parts import Part, SeqPosition
 
 class SuperGSLCoreFixtures(object):
 
     def get_assembly_ast(self):
         parts = [
-            Part('uHO'),
-            Part('pADH1'),
-            Part('gERG10'),
-            Part('tADH1'),
-            Part('dHO'),
+            AstPart('uHO'),
+            AstPart('pADH1'),
+            AstPart('gERG10'),
+            AstPart('tADH1'),
+            AstPart('dHO'),
         ]
         assembly = Assembly(parts)
 
         return assembly
 
+    def mk_random_dna_sequence(self, seq_len):
+        seq_str = ''.join(random.choice('CGTA') for _ in range(seq_len))
+        return Seq(seq_str)
+
+    def mk_part(self, identifier, part_seq_len):
+        """Create a mock Part.
+
+        Part is derived from a reference sequence three times longer than the part
+        itself.
+        """
+        ref_seq_len = part_seq_len * 3
+        reference_sequence = self.mk_random_dna_sequence(ref_seq_len)
+
+        start = SeqPosition.from_reference(
+            x=part_seq_len,
+            rel_to=THREE_PRIME,
+            approximate=False,
+            reference=reference_sequence
+        )
+        end = start.get_relative_position(x=part_seq_len)
+
+        part = Part(
+            'TESTPART',
+            start,
+            end,
+            provider=mock.Mock()
+        )
+        return reference_sequence, part
