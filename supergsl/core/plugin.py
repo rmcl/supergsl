7223d1c054e44ab17e6ba6df8b945e13b1f8a28a
Russell McLoughlin <russ.mcl@gmail.com>
2021-02-07 10:03:46 -0800

    fix mypy errors.

--- supergsl/core/plugin.py
+++ supergsl/core/plugin.py
@@ -8,7 +8,34 @@ from supergsl.core.exception import ConfigurationException
 from supergsl.core.symbol_table import SymbolTable
 
 class SuperGSLPlugin(object):
-    """Base class for defining a SuperGSL Plugin."""
+    """Base class for defining a SuperGSL Plugin.
+
+    An example of writing a function for SuperGSL:
+
+    class ExamplePlugin(SuperGSLPlugin):
+
+        # Types of plugins
+        #    - SuperGSL Function
+
+
+        def register(self, context):
+            cut_func = FunctionRegistration(
+                'cut', self.cut, return_value=sgsl_types.PART_LIST, help=self.cut.__docstr__)
+
+            cut_fun.add_argument('target_gene', sgsl_types.PART)
+            cut_fun.add_argument('target_genome', sgsl_types.GENOME)
+            cut_fun.add_argument('target_genome', sgsl_types.GENOME)
+            cut_fun.add_argument('num_results', int)
+
+        def cut(self, sgsl_args):
+            ""
+            Generate gRNA for CRISPR/Cas9
+
+            Example sGSL syntax:
+                from example import cut
+                cut(HO, CAS9, S288C, results=5)
+            ""
+    """
 
     def register(self, symbol_table):
         """Register Functions, enums, etc that the plugin provides.
