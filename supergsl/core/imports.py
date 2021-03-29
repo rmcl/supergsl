d4b146330a126d4582b634bfe67f99305cbd8433
Russell McLoughlin <russ.mcl@gmail.com>
2021-03-29 09:04:42 -0700

    add docs and missing exception.

--- supergsl/core/imports.py
+++ supergsl/core/imports.py
@@ -1,8 +1,11 @@
 from supergsl.core.backend import BreadthFirstNodeFilteredPass
 from supergsl.core.parts import Part, LazyLoadedPart
+from supergsl.core.function import SuperGSLFunction
+from supergsl.core.exception import SuperGSLTypeError
 
 
 class ResolveImportsPass(BreadthFirstNodeFilteredPass):
+    """Traverse the AST and attempt to resolve imported symbols."""
 
     def get_node_handlers(self):
         return {
@@ -15,7 +18,7 @@ class ResolveImportsPass(BreadthFirstNodeFilteredPass):
         return ast
 
     def visit_import_node(self, node):
-
+        """Visit AST import nodes and resolve them via the registered providers."""
         import_table = self.symbol_table.nested_scope('imports')
 
         for program_import in node.imports:
@@ -30,7 +33,12 @@ class ResolveImportsPass(BreadthFirstNodeFilteredPass):
         return node
 
     def visit_function_invoke_node(self, node):
-        #node.function = self.symbol_table.get_symbol(node.identifier)
+        node.function = self.symbol_table.lookup(node.identifier)
+        if not isinstance(node.function, SuperGSLFunction):
+            raise SuperGSLTypeError('{} is not of type Function. It is a "{}"'.format(
+                node.identifier,
+                type(node.function)
+            ))
 
         ## TODO: DO TYPE CHECKING TO ASSERT THIS SYMBOL IS A FUNCTION
         #node.expected_type = node.function.get_return_type()
