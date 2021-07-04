f698c3faecd6abc65cf6a36a8bbf7185661140ef
Russell McLoughlin <russ.mcl@gmail.com>
2021-07-04 13:30:15 -0700

    update entrypoint.

--- supergsl/__init__.py
+++ supergsl/__init__.py
@@ -1,9 +1,10 @@
+"""Define a minimal entrypoint to initialize the supergsl interpreter."""
 from typing import Optional, List
 from .core.config import load_settings
 from .core.pipeline import CompilerPipeline
 
 
-def get_compiler(config_files : Optional[List[str]] = None):
+def get_interpreter(config_files : Optional[List[str]] = None):
     """Retrieve an instance of the SuperGSL compiler."""
     compiler_settings = load_settings(config_files)
     return CompilerPipeline(compiler_settings)
