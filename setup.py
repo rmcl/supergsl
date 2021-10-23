052d2afcf18d98c138a7100c0b31719b0e39753d
Russell McLoughlin <russ.mcl@gmail.com>
2021-10-23 11:47:24 -0700

    bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="supergsl",
-    version="0.7.2",
+    version="0.8.0",
     author="Russell McLoughlin",
     author_email="russ.mcl@gmail.com",
     description="A python implementation of the Genome Specification Language (GSL) for genetic engineering.",
